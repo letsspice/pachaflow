61040709e3485a7832e0d2de8fe8deadf06ee8c9
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-02-21 21:54:51 +0300

    Add the import to the Activities component and added styling for the activities section in the globals.css file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import StayWithUs from "./components/StayWithUs";
+import Activities from "./components/Activities";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <StayWithUs />
+      <Activities />
     </>
   );
 }
