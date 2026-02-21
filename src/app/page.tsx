a1b6cd0a847469d8b51dd1b901b37cecffe40853
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-02-21 20:20:06 +0300

    Add the images for the stay with us section and added the StayWithUs component to the main file of the app to be tracked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import StayWithUs from "./components/StayWithUs";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <StayWithUs />
     </>
   );
 }
