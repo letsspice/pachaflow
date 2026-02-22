908b8f7e88dce13b5fa7d4dc9cd4a854c8323e49
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-02-22 23:06:36 +0300

    Add the RiverView CTA image and added the component to the main entry point of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import StayWithUs from "./components/StayWithUs";
 import Activities from "./components/Activities";
 import DineByRiver from "./components/DineByRiver";
+import RiverviewCTA from "./components/RiverviewCTA";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <StayWithUs />
       <Activities />
       <DineByRiver />
+      <RiverviewCTA />
     </>
   );
 }
